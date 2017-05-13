50cc5cbcbbf9abb052b45c55d7db9d0fcb55133b
Alun Davey <hey@4lun.io>
2017-05-13 18:29:06 +0100

    Depth first search on Explore (need to implement backtracking when stuck)

--- src/main/java/student/Explore.java
+++ src/main/java/student/Explore.java
@@ -5,7 +5,20 @@ import java.util.*;
 
 public class Explore {
 
+  /**
+   * Contains the current game state
+   */
   private ExplorationState state;
+
+  /**
+   * Keeps track of all visited nodes
+   */
+  private HashSet<NodeStatus> visitedNodes;
+
+  /**
+   * Keeps track of current path
+   */
+  private Stack<NodeStatus> pathHistory;
 
   /**
    * Init Explore instance
@@ -14,16 +27,24 @@ public class Explore {
    */
   public Explore(ExplorationState state) {
     this.state = state;
+    this.visitedNodes = new HashSet<>();
+    this.pathHistory = new Stack<>();
   }
 
   /**
    * Explore the cavern
    */
   public void go() {
+    NodeStatus targetNode;
+
     while(state.getDistanceToTarget() != 0) {
-      moveToNeighbour(
-        getRandomNeighbour()
-      );
+      targetNode = getRandomUnvisitedNeighbour();
+
+      if(targetNode == null) {
+        targetNode = backtrackToUnvisitedNeighbour();
+      }
+
+      moveToNeighbour(targetNode);
     }
   }
 
@@ -34,8 +55,61 @@ public class Explore {
    */
   private NodeStatus getRandomNeighbour() {
     Collection<NodeStatus> neighbours = state.getNeighbours();
+    NodeStatus randomNeighbour = randomNeighbour(neighbours);
 
-    if(!neighbours.isEmpty()) {
+    if(randomNeighbour == null) {
+      throw new IllegalStateException("We are trapped.");
+    }
+
+    return randomNeighbour;
+  }
+
+  /**
+   * Get a random neighbour on the current state that we haven't yet visited
+   *
+   * @return random unvisited neighbour or null if there aren't any
+   */
+  private NodeStatus getRandomUnvisitedNeighbour() {
+    Collection<NodeStatus> neighbours = state.getNeighbours();
+
+    // Filter out visited neighbours
+    neighbours.removeIf(
+      (NodeStatus neighbour) -> this.visitedNodes.contains(neighbour)
+    );
+
+    return randomNeighbour(neighbours);
+  }
+
+  /**
+   * Backtrack until we get to a tile with an unvisited neighbour
+   *
+   * @return random unvisited neighbour
+   */
+  private NodeStatus backtrackToUnvisitedNeighbour() {
+    return null;
+  }
+
+  /**
+   * Move to specified neighbour
+   *
+   * @param NodeStatus neighbour to move to
+   */
+  private void moveToNeighbour(NodeStatus neighbour) {
+    state.moveTo(neighbour.getId());
+    this.visitedNodes.add(neighbour);
+    this.pathHistory.push(neighbour);
+  }
+
+  /**
+   * Returns a random neighbour from the provided collection
+   *
+   * @param  neighbours Collection of neighbours to select from
+   *
+   * @return a random neighbour from the provided collection
+   */
+  private NodeStatus randomNeighbour(Collection<NodeStatus> neighbours) {
+    if (!neighbours.isEmpty()) {
+      System.out.println(neighbours.size());
       int randomNeighbour = new Random().nextInt(neighbours.size());
       int neighbourI = 0;
 
@@ -47,15 +121,6 @@ public class Explore {
       }
     }
 
-    throw new IllegalStateException("We are trapped.");
-  }
-
-  /**
-   * Move to specified neighbour
-   *
-   * @param NodeStatus neighbour to move to
-   */
-  private void moveToNeighbour(NodeStatus neighbour) {
-    state.moveTo(neighbour.getId());
+    return null;
   }
 }
